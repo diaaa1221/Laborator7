9305e67368822f0001295e17357d19ffcdc384be
DIANA-MARIA ONEȚIU <diana.onetiu@stud.ubbcluj.ro>
2024-12-06 16:48:20 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace OnetiuDianaLab7
+﻿using System;
+using OnetiuDianaLab7.Data;
+using System.IO;
+namespace OnetiuDianaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
