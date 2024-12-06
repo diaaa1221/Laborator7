c15fce848b7b68446b0e5b0a758b05ddb0100a9c
DIANA-MARIA ONEȚIU <diana.onetiu@stud.ubbcluj.ro>
2024-12-06 17:59:19 +0200

    Lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,30 @@ async void OnDeleteButtonClicked(object sender, EventArgs e)
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+
+
+async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
